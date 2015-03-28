5cf595b5a2a5ffdb7f3133f8a2420d604362eac1
John Sandall <john.sandall@gmail.com>
2015-03-28 16:38:45 +0000

    Four shortcuts for changing status on Maniphest tasks (resolved, wontfix, invalid, spite, open)

---
+++ shortcuts.js
@@ -0,0 +1,64 @@
+// ==UserScript==
+// @ShortcutManager
+// @name Maniphest - status resolved
+// @namespace iv4aMErvzrgV
+// @key s r
+// @include https://phabricator.yplanapp.com/*
+// ==/UserScript==
+javascript:
+document.getElementById('transaction-action').value = 'status';
+document.getElementById('resolution').style['display'] = '';
+document.getElementById('UQ0_4').value = 'resolved';
+document.querySelectorAll("[action='/maniphest/transaction/save/']")[0].submit();
+
+// ==UserScript==
+// @ShortcutManager
+// @name Maniphest - status wontfix
+// @namespace iv4aMErvzrgV
+// @key s w
+// @include https://phabricator.yplanapp.com/*
+// ==/UserScript==
+javascript:
+document.getElementById('transaction-action').value = 'status';
+document.getElementById('resolution').style['display'] = '';
+document.getElementById('UQ0_4').value = 'wontfix';
+document.querySelectorAll("[action='/maniphest/transaction/save/']")[0].submit();
+
+// ==UserScript==
+// @ShortcutManager
+// @name Maniphest - status invalid
+// @namespace iv4aMErvzrgV
+// @key s i
+// @include https://phabricator.yplanapp.com/*
+// ==/UserScript==
+javascript:
+document.getElementById('transaction-action').value = 'status';
+document.getElementById('resolution').style['display'] = '';
+document.getElementById('UQ0_4').value = 'invalid';
+document.querySelectorAll("[action='/maniphest/transaction/save/']")[0].submit();
+
+// ==UserScript==
+// @ShortcutManager
+// @name Maniphest - status spite
+// @namespace iv4aMErvzrgV
+// @key s s
+// @include https://phabricator.yplanapp.com/*
+// ==/UserScript==
+javascript:
+document.getElementById('transaction-action').value = 'status';
+document.getElementById('resolution').style['display'] = '';
+document.getElementById('UQ0_4').value = 'spite';
+document.querySelectorAll("[action='/maniphest/transaction/save/']")[0].submit();
+
+// ==UserScript==
+// @ShortcutManager
+// @name Maniphest - status open
+// @namespace iv4aMErvzrgV
+// @key s o
+// @include https://phabricator.yplanapp.com/*
+// ==/UserScript==
+javascript:
+document.getElementById('transaction-action').value = 'status';
+document.getElementById('resolution').style['display'] = '';
+document.getElementById('UQ0_4').value = 'open';
+document.querySelectorAll("[action='/maniphest/transaction/save/']")[0].submit();
