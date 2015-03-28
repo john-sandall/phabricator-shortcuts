8fe20163f4c87ae1f8d45ecece54a8a753ef9351
John Sandall <john.sandall@gmail.com>
2015-03-28 16:56:44 +0000

    Added a nice readme

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# phabricator-shortcuts
-Keyboard shortcuts for Phabricator, using the Shortcut Manager extension for Google Chrome
+## Keyboard shortcuts for Phabricator
+
+[Phabricator](http://phabricator.org) is a suite of open source tools for peer code review, task management, and project communication. Its keyboard shortcuts are generally pretty terrible, especially when compared to other web apps for task management.
+
+This project uses the [Shortcut Manager extension](https://chrome.google.com/webstore/detail/shortcut-manager/mgjjeipcdnnjhgodgjpfkffcejoljijf) for Google Chrome.
+
+####Instructions
+1. Get the [Shortcut Manager extension](https://chrome.google.com/webstore/detail/shortcut-manager/mgjjeipcdnnjhgodgjpfkffcejoljijf).
+2. Go to the settings page.
+3. Click "Import settings".
+4. Select all, then copy/paste everything from this file into the textbox: `https://raw.githubusercontent.com/john-sandall/phabricator-shortcuts/master/shortcuts.js`
+5. Hit "Done", hopefully it worked!
+
+####Examples
+**Change status in Maniphest:**
+
+  * Typing `s r` (`s` then `r`) closes task as resolved
+  * Typing `s w` (`s` then `r`) closes task as wontfix
+  * Typing `s i` (`s` then `r`) closes task as invalid
+  * Typing `s s` (`s` then `r`) closes task as spite
+  * Typing `s o` (`s` then `r`) reopens the task
+
+####Caveats
+These shortcuts will only work for you if your Phabricator instance is hosted on a subdomain, i.e. URL looks like `https://phabricator.example.com`. If this is not true you will need to change the "URL patterns" field on each shortcut.
--- shortcuts.js
+++ shortcuts.js
@@ -1,9 +1,18 @@
 // ==UserScript==
 // @ShortcutManager
+// @name 
+// @namespace iv4aMErvzrgV
+// @key 
+// @include *
+// @execute OpenAPage()
+// ==/UserScript==
+
+// ==UserScript==
+// @ShortcutManager
 // @name Maniphest - status resolved
 // @namespace iv4aMErvzrgV
 // @key s r
-// @include https://phabricator.yplanapp.com/*
+// @include https://phabricator.*
 // ==/UserScript==
 javascript:
 document.getElementById('transaction-action').value = 'status';
@@ -11,12 +20,14 @@ document.getElementById('resolution').style['display'] = '';
 document.getElementById('UQ0_4').value = 'resolved';
 document.querySelectorAll("[action='/maniphest/transaction/save/']")[0].submit();
 
+
+
 // ==UserScript==
 // @ShortcutManager
 // @name Maniphest - status wontfix
 // @namespace iv4aMErvzrgV
 // @key s w
-// @include https://phabricator.yplanapp.com/*
+// @include https://phabricator.*
 // ==/UserScript==
 javascript:
 document.getElementById('transaction-action').value = 'status';
@@ -24,12 +35,14 @@ document.getElementById('resolution').style['display'] = '';
 document.getElementById('UQ0_4').value = 'wontfix';
 document.querySelectorAll("[action='/maniphest/transaction/save/']")[0].submit();
 
+
+
 // ==UserScript==
 // @ShortcutManager
 // @name Maniphest - status invalid
 // @namespace iv4aMErvzrgV
 // @key s i
-// @include https://phabricator.yplanapp.com/*
+// @include https://phabricator.*
 // ==/UserScript==
 javascript:
 document.getElementById('transaction-action').value = 'status';
@@ -37,12 +50,14 @@ document.getElementById('resolution').style['display'] = '';
 document.getElementById('UQ0_4').value = 'invalid';
 document.querySelectorAll("[action='/maniphest/transaction/save/']")[0].submit();
 
+
+
 // ==UserScript==
 // @ShortcutManager
 // @name Maniphest - status spite
 // @namespace iv4aMErvzrgV
 // @key s s
-// @include https://phabricator.yplanapp.com/*
+// @include https://phabricator.*
 // ==/UserScript==
 javascript:
 document.getElementById('transaction-action').value = 'status';
@@ -50,12 +65,14 @@ document.getElementById('resolution').style['display'] = '';
 document.getElementById('UQ0_4').value = 'spite';
 document.querySelectorAll("[action='/maniphest/transaction/save/']")[0].submit();
 
+
+
 // ==UserScript==
 // @ShortcutManager
 // @name Maniphest - status open
 // @namespace iv4aMErvzrgV
 // @key s o
-// @include https://phabricator.yplanapp.com/*
+// @include https://phabricator.*
 // ==/UserScript==
 javascript:
 document.getElementById('transaction-action').value = 'status';
